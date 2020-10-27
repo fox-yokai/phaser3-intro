f184d8f40547c5b71581901230a7a11329c74837
fox-yokai <pat.w.cain@gmail.com>
2020-10-27 14:44:39 -0500

    basic logic created

---
+++ public/assets/js/scences/GameScene.js
@@ -0,0 +1,47 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('Game');
+    }
+
+    create() {
+        var goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2 });
+
+        let button = this.add.image(100, 100, 'button1');
+        button.setOrigin(0.5, 0.5);
+    
+        this.add.sprite(300, 100, 'button1');
+    
+        this.chest = this.physics.add.image(300, 300, 'items', 0);
+    
+        this.wall = this.physics.add.image(500, 100, 'button1');
+        this.wall.setImmovable();
+    
+        this.player = this.physics.add.image(32, 32, 'characters', 0);
+        this.player.setScale(2);
+        this.player.body.setCollideWorldBounds(true);
+    
+        this.physics.add.collider(this.player, this.wall);
+        this.physics.add.overlap(this.player, this.chest, function(player, chest) {
+            goldPickupAudio.play();
+            chest.destroy();
+            });
+    
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        this.player.setVelocity(0);
+
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+        }
+    
+        if (this.cursors.up.isDown) {
+            this.player.setVelocityY(-160);
+        } else if (this.cursors.down.isDown) {
+            this.player.setVelocityY(160);
+        }
+    }
+}
--- public/game.js
+++ public/game.js
@@ -2,18 +2,21 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    scene: {
-        preload,
-        create,
+    scene: [
+        BootScene,
+        TitleScene,
+        GameScene,
+        UiScene,
+    ],
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                y: 0,
+            }
+        },
     },
 };
 
 var game = new Phaser.Game(config);
-
-function preload() {
-
-};
-
-function create() {
-
-};
