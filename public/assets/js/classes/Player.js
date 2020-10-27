678ce68c40363b5c42444a2169e14dc58ef33b3d
fox-yokai <pat.w.cain@gmail.com>
2020-10-27 15:28:52 -0500

    Player class created

---
+++ public/assets/js/classes/Player.js
@@ -0,0 +1,34 @@
+class Player extends Phaser.Physics.Arcade.Image {
+    constructor(scene, x, y, key, frame) {
+        super(scene, x, y, key, frame);
+        this.scene = scene;
+        this.velocity = 160
+
+        // enable physics
+        this.scene.physics.world.enable(this);
+        // set player immovable if another object collides
+        this.setImmovable(false);
+        // scale the player
+        this.setScale(2);
+        // collide with world bounds
+        this.setCollideWorldBounds(true);
+        // add the player to the existing scene
+        this.scene.add.existing(this);
+    }
+
+    update(cursors) {
+        this.body.setVelocity(0);
+
+        if (cursors.left.isDown) {
+            this.body.setVelocityX(-this.velocity);
+        } else if (cursors.right.isDown) {
+            this.body.setVelocityX(this.velocity);
+        }
+    
+        if (cursors.up.isDown) {
+            this.body.setVelocityY(-this.velocity);
+        } else if (cursors.down.isDown) {
+            this.body.setVelocityY(this.velocity);
+        }
+    }
+}
