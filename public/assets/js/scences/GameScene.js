3347fba54e7d4fa5c25ba61f25e5c2ab2973af01
fox-yokai <pat.w.cain@gmail.com>
2020-10-30 17:42:49 -0500

    Created Ui to track coins

--- public/assets/js/scences/GameScene.js
+++ public/assets/js/scences/GameScene.js
@@ -3,8 +3,12 @@ class GameScene extends Phaser.Scene {
         super('Game');
     }
 
+    init() {
+        this.scene.launch('Ui');
+    }
+
     create() {
-        var goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2 });
+        this.goldPickupAudio = this.sound.add('goldSound', { loop: false, volume: 0.2 });
     
         this.chest = new Chest(this, 300, 300, 'items', 0);
     
@@ -14,15 +18,21 @@ class GameScene extends Phaser.Scene {
         this.player = new Player(this, 32, 32, 'characters', 0);
     
         this.physics.add.collider(this.player, this.wall);
-        this.physics.add.overlap(this.player, this.chest, function(player, chest) {
-            goldPickupAudio.play();
-            chest.destroy();
-            });
+        this.physics.add.overlap(this.player, this.chest, this.collectChest, null, this);
     
         this.cursors = this.input.keyboard.createCursorKeys();
     }
 
     update() {
         this.player.update(this.cursors);
+    }
+
+    collectChest(player, chest) {
+        // play gold pickup sound
+        this.goldPickupAudio.play();
+        // update score in the ui
+        this.events.emit('updateScore', chest.coins);
+        // destroy the chest game object
+        chest.destroy();
     }
 }
